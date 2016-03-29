b68b130d53d39f45301e79512dad91fad2564602
Stefan Schörghofer <amd1212@vier-ringe.at>
2016-03-29 18:04:06 +0200

    Initial commit - will be tested now by Darth Maul and maybe adopted if there are errors

---
+++ Boom-Boom-izer.sh
@@ -0,0 +1,37 @@
+/bin/sh
+##################################
+# Vars
+copies=0
+
+echo "Boom Boom Boom Potty Boom!"
+echo "##########################"
+echo "01 - Installing Packages we need"
+
+# Package installations
+apt update && apt -y install omxplayer i3 lightdm
+if [ $? -ne 0 ] ; then
+	echo " - BOOM - There was a Boom Boom problem with Boom Boom package installation."
+	echo " - BOOM - Do you have an Network/Internet connection?"
+	echo " - BOOM - Please start the Boom-Boom-izer.sh again when your Boom Boom InternetZ is working"
+	exit 42
+fi
+
+# Adopting the configs
+echo "02 - Changing some configs"
+cp /tmp/potty-boom-boom/potty-lightdm.conf /etc/lightdm/lightdm.conf && ((copies=${copies}+1)) && echo " - Installed lightdm.conf for pi-autologin"
+mkdir /home/pi/.i3
+cp /tmp/potty-boom-boom/potty-i3.conf /home/pi/.i3/config && ((copies=${copies}+1)) && echo " - Installed i3 config, starts the watchdog"
+mkdir -p /home/pi/Potty-Boom-Boom/Files
+chown -R pi:pi /home/pi/Potty-Boom-Boom
+cp /tmp/potty-boom-boom/Potty-Boom-Boom-watchdog.sh /home/Potty-Boom-Boom/Potty-Boom-Boom-watchdog.sh && ((copies=${copies}+1)) && echo " - Installed the watchdog, looks for videos and takes a random one for the player"
+chmod +x /home/Potty-Boom-Boom/Potty-Boom-Boom-watchdog.sh && echo " - Watchdog is now executeable"
+
+if [ ${copies} -lt 3 ] ; then
+	echo " - BOOM - There was a problem with Boom Boom copying Boom files"
+	echo " - BOOM - Maybe the guy who Boom Boom wrote the script had to less Boom Boom sleep"
+	exit 42
+fi
+
+# END
+echo "03 - Profit (Potty-Boom-Boom should Boom Boom after compatible files are placed in the correct folder and a reboot)
+exit 0
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# potty-boom-boom
+# Potty-Boom-Boom
 An raspian based Toilet-media-center used at EH16 to entertain and speed up toilet usage
+
+HOWTO:
+ - Deploy Raspian-Lite[1] on a SD Card of your joice (If the Potty-Boom-Boom is used without network and remote-storage choose a big SD-Card 8GB+) 
+ - Start your new Potty-Boom-Boom device and connect to it.
+ - Install git, checkout the Boom-Boom-izer.sh and execute it.
+```
+apt update && apt -y install git
+git clone https://github.com/AMD1212/potty-boom-boom.git /tmp/potty-boom-boom
+sh /tmp/potty-boom-boom/Boom-Boom-izer.sh
+```
+ - Place your mp4 encoded videos in the /home/pi/Potty-Boom-Boom/Files/
+ - Run raspi-config and choose the menupoint to extend the root-filesystem and reboot the system
+ - Boom Boom!
+
+
+
+
+
+
+[1] https://www.raspberrypi.org/downloads/raspbian/
