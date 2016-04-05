7aa45fed349ba60308b0fff8f3bed723a6206e73
Pepi Zawodsky <git@maclemon.at>
2016-04-05 02:15:24 +0200

    Removed quotes from find command, doh

--- Potty-Boom-Boom-watchdog.sh
+++ Potty-Boom-Boom-watchdog.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-        myrandomfile=$(find "~/Potty-Boom-Boom/Files/" -type f | shuf -n 1)
+        myrandomfile=$(find ~/Potty-Boom-Boom/Files/ -type f | shuf -n 1)
         omxplayer -b "${myrandomfile}"
 done
