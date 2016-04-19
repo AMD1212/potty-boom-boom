d50c370d4ef9796cca30a8532377ebca35b41cf7
Hans-Peter Oeri <hp@oeri.ch>
2016-04-19 21:02:02 +0200

    implement second stage install

--- Boom-Boom-izer.sh
+++ Boom-Boom-izer.sh
@@ -46,44 +46,96 @@ EOF
 	exit
 fi
 
-## STAGE 2: original install
-# Vars
-copies=0
+## STAGE 2: full size install
 
-echo "Boom Boom Boom Potty Boom!"
-echo "##########################"
-echo "01 - Installing Packages we need"
-
-# Package installations
-apt update && apt -y install omxplayer i3 lightdm youtube-dl
-if [ $? -ne 0 ] ; then
-	echo " - BOOM - There was a Boom Boom problem with Boom Boom package installation."
-	echo " - BOOM - Do you have an Network/Internet connection?"
-	echo " - BOOM - Please start the Boom-Boom-izer.sh again when your Boom Boom InternetZ is working"
+function err_apt {
+	whiptail --title "ERROR" --infobox "Package installation failed - \
+is there a network available? Please connect and restart..." 8 78
 	exit 42
-fi
+}
 
-# Adopting the configs
-echo "02 - Changing some configs"
-cp -f /tmp/potty-boom-boom/potty-lightdm.conf /etc/lightdm/lightdm.conf && copies=$((copies + 1 )) && echo " - Installed lightdm.conf for pi-autologin"
-mkdir -p /home/pi/.i3
-cp -f /tmp/potty-boom-boom/potty-i3.conf /home/pi/.i3/config && copies=$((copies + 1 )) && echo " - Installed i3 config, starts the watchdog"
+function err_common {
+	whiptail --title "ERROR" --infobox "there was a problem - \
+Maybe I had too little boom boom sleep?" 8 78
+	exit 42
+}
+
+chvt 2
+echo "second stage install taking off..."
+setterm -blank 0 -powersave off
+
+trap "err_apt" ERR
+debconf-apt-progress -- apt update -y
+debconf-apt-progress -- apt install -y omxplayer i3 lightdm youtube-dl
+whiptail --title "youtube-dl" --infobox "patch in the latest youtube-dl..." 8 78
+wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl 2> /dev/null
+chmod a+rx /usr/local/bin/youtube-dl
+
+whiptail --title "configuration" --infobox "installing configs..." 8 78
 mkdir -p /home/pi/Potty-Boom-Boom/Files
-chown -R pi:pi /home/pi/Potty-Boom-Boom
-cp -f /tmp/potty-boom-boom/Potty-Boom-Boom-watchdog.sh /home/pi/Potty-Boom-Boom/Potty-Boom-Boom-watchdog.sh && copies=$((copies + 1 )) && echo " - Installed the watchdog, looks for videos and takes a random one for the player"
-chmod +x /home/pi/Potty-Boom-Boom/Potty-Boom-Boom-watchdog.sh && echo " - Watchdog is now executeable"
+cp Potty-Boom-Boom-watchdog.sh /home/pi/Potty-Boom-Boom/
 
-if [ ${copies} -lt 3 ] ; then
-	echo " - BOOM - There was a problem with Boom Boom copying Boom files"
-	echo " - BOOM - Maybe the guy who Boom Boom wrote the script had to less Boom Boom sleep"
-	exit 42
+tee /etc/lightdm/lightdm.conf > /dev/null <<EOF
+[SeatDefaults]
+autologin-user=pi
+autologin-user-timeout=0
+
+[XDMCPServer]
+
+[VNCServer]
+EOF
+
+mkdir -p /home/pi/.i3
+tee /home/pi/.i3/config > /dev/null <<EOF
+# i3 config file (v4)
+exec_always /home/pi/Potty-Boom-Boom/Potty-Boom-Boom-watchdog.sh
+EOF
+chown -R pi.pi /home/pi/.i3
+
+PLAYLIST=`cat PLAYLIST`
+if [[ -n "$PLAYLIST" ]]; then 
+	# we have to play with mawk as it is debian standard :(
+	(
+	cd /home/pi/Potty-Boom-Boom/Files/
+	youtube-dl --prefer-free-formats --newline --format 'mp4[height<=720]' \
+		"$PLAYLIST"
+	) | /usr/bin/mawk -W interactive '
+function ltrim(s) { sub(/^[ \t\r\n]+/, "", s); return s }
+function rtrim(s) { sub(/[ \t\r\n]+$/, "", s); return s }
+function trim(s)  { return rtrim(ltrim(s)); }
+BEGIN {
+        current=0;
+        total=0;
+        videoname="";
+        print "HALLO";
+}
+/\[download\] Downloading video/ {
+        current=$4;
+        total=$6;
+print "A";
+}
+/\[download\] Destination:/ {
+        $1="";
+        $2="";
+        videoname=trim($0);
+print "B";
+}
+/\[download\] +[0-9.]+% of/ {
+        printf( "XXX\n%d\nVideo %d/%d: %s (ETA %s)\nXXX\n",
+                (100/total)*(current-1+int($2)/100), current, total, videoname, $8 );
+}
+' > >( whiptail --title "Download Initial Playlist" --gauge "starting download..." 8 70 0 )
+
 fi
 
-# Prefill with ONE Video
-youtube-dl --prefer-free-formats --audio-format best --format mp4 --output /home/pi/Potty-Boom-Boom/Files/01_Pausenmusik https://www.youtube.com/watch?v=C23E5grsczE 
+chown -R pi:pi /home/pi/Potty-Boom-Boom
+amixer sset PCM,0 100 > /dev/null
+alsactl store
 
-# END
-echo "03 - Profit (Potty-Boom-Boom should Boom Boom after compatible files are placed in the correct folder and a reboot)"
-echo "DON'T FORGET TO SET A PROPER VOLUME USING ALSAMIXER"
+# remove ourselves
+systemctl disable potty-boom-install > /dev/null
+rm /etc/systemd/system/potty-boom-install.service
 
+shutdown -r now
 exit 0
+
